850e12daabcba72f4dc3be9a3da62ecbb2fd19fa
hakkulinen <rmb4@ro.ru>
2025-05-28 01:39:47 +0300

    add useful flip func

--- config.py
+++ config.py
@@ -6,5 +6,10 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s"
 )
+
+def flip(flipper):
+    flipper = (flipper + 1) % 2  # Toggles between 0 and 1
+    return flipper
+
 # Debug flag
 DEBUG = False  # Set to False to disable debug logs
