5c2b206c9bf239b2a36f738bd1a01e0c9b5e19b7
hakkulinen <rmb4@ro.ru>
2025-06-01 02:12:32 +0300

    change MAX_DEFECT_LENGHT to 20

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ logging.basicConfig(
     format="%(asctime)s - %(levelname)s - %(message)s"
 )
 #int value for the logic of printing the defects in the top right of the image. Might change with new defects being added
-MAX_DEFECT_LENGTH = 17
+MAX_DEFECT_LENGTH = 20
 
 def flip(flipper):
     flipper = (flipper + 1) % 2  # Toggles between 0 and 1
